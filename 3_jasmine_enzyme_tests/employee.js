de920722074f7eee596f3c0f2125b4f09de48ea7
planktonlex55 <planktonlex55@gmail.com>
2017-06-19 13:06:00 +0530

    Create employee.js

---
+++ 3_jasmine_enzyme_tests/employee.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// functional_stateless_component
+export const employee = () => {
+return(<h1 className="h1class"> I am a functional_stateless_component </h1>)
+}
+
+export const Home = () => {
+  return (<button className ="pclass" disabled >inside home</button>);
+}
